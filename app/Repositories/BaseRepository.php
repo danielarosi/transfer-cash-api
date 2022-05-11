82f42ce858c43c4f148c5c7d51428195518d389b
danielarosi <danielars.br@gmail.com>
2022-05-11 14:03:59 -0300

    BaseRepository criado

---
+++ app/Repositories/BaseRepository.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Repositories;
+
+class BaseRepository
+{
+    protected $obj;
+
+    protected function __construct(object $obj)
+    {
+        $this->obj = $obj;
+    }
+
+    public function all(): object
+    {
+        return $this->obj->all();
+    }
+
+    public function find(int $id): object
+    {
+        return $this->obj->find($id);
+    }
+
+    public function findFirstByColumn(string $column, $value)
+    {
+        return $this->obj->where($column, $value)->first();
+    }
+    
+    public function findAllByColumn(string $column, $value)
+    {
+        return $this->obj->where($column, $value)->get();
+
+    }
+    public function save(array $attributes): object
+    {
+        return $this->obj->create($attributes);
+    }
+    
+}
